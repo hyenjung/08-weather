0def07be8b6af3c9c73365b3a325abd4f7ad9759
hyenjung <beibby@naver.com>
2021-01-04 13:11:20 +0900

    dsds

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+// 웹사이트 주소체계: https://nodejs.org/dist/latest-v15.x/docs/api/url.html
+/*
+// AJAX 통신
+var url = 'https://api.openweathermap.org/data/2.5/onecall?lat=38&lon=127&appid=02efdd64bdc14b279bc91d9247db4722&units=metric&exclude=minutely,hourly';
+function onGet(r) {
+	console.log(r);
+}
+
+$("#bt").click(function(){
+	$.get(url, onGet);
+});
+*/
+
+// openweathermap : c2d43350047233a6723130800903619c
+// kakao : fc20071e530e3ffa3b5ff6ac9388f5d4
+
+// 7days https://api.openweathermap.org/data/2.5/onecall?lat=38&lon=127&appid=c2d43350047233a6723130800903619c&units=metric&exclude=minutely,hourly
